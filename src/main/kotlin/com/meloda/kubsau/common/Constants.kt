ab5040b85650c5b8a83ed2b18951b72dbdd33cca
Danil Nikolaev <lischenkodev@gmail.com>
2024-04-14 21:24:55 +0300

    version upstream

--- src/main/kotlin/com/meloda/kubsau/common/Constants.kt
+++ src/main/kotlin/com/meloda/kubsau/common/Constants.kt
@@ -2,7 +2,7 @@ package com.meloda.kubsau.common
 
 object Constants {
 
-    const val BACKEND_VERSION = "0.0.8"
+    const val BACKEND_VERSION = "0.0.9"
 }
 
 val isInDocker = System.getenv("IS_DOCKER") == "true"
