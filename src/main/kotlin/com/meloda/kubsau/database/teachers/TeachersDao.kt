ff3e1e3080da22b0e2f00fbb50620445201e124a
Danil Nikolaev <lischenkodev@gmail.com>
2024-04-24 17:46:14 +0300

    CRUD for teachers

--- src/main/kotlin/com/meloda/kubsau/database/teachers/TeachersDao.kt
+++ src/main/kotlin/com/meloda/kubsau/database/teachers/TeachersDao.kt
@@ -6,7 +6,9 @@ import com.meloda.kubsau.model.Teacher
 interface TeachersDao : Dao<Teacher> {
 
     suspend fun allTeachers(): List<Teacher>
+    suspend fun allTeachersByIds(teacherIds: List<Int>): List<Teacher>
     suspend fun singleTeacher(teacherId: Int): Teacher?
+
     suspend fun addNewTeacher(
         firstName: String,
         lastName: String,
@@ -14,5 +16,14 @@ interface TeachersDao : Dao<Teacher> {
         departmentId: Int
     ): Teacher?
 
+    suspend fun updateTeacher(
+        teacherId: Int,
+        firstName: String,
+        lastName: String,
+        middleName: String,
+        departmentId: Int
+    ): Int
+
     suspend fun deleteTeacher(teacherId: Int): Boolean
+    suspend fun deleteTeachers(teacherIds: List<Int>): Boolean
 }
--- src/main/kotlin/com/meloda/kubsau/database/teachers/TeachersDaoImpl.kt
+++ src/main/kotlin/com/meloda/kubsau/database/teachers/TeachersDaoImpl.kt
@@ -2,15 +2,21 @@ package com.meloda.kubsau.database.teachers
 
 import com.meloda.kubsau.database.DatabaseController.dbQuery
 import com.meloda.kubsau.model.Teacher
-import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
-import org.jetbrains.exposed.sql.deleteWhere
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.inList
 
 class TeachersDaoImpl : TeachersDao {
+
     override suspend fun allTeachers(): List<Teacher> = dbQuery {
         Teachers.selectAll().map(::mapResultRow)
+    }
+
+    override suspend fun allTeachersByIds(teacherIds: List<Int>): List<Teacher> = dbQuery {
+        Teachers
+            .selectAll()
+            .where { Teachers.id inList teacherIds }
+            .map(::mapResultRow)
     }
 
     override suspend fun singleTeacher(teacherId: Int): Teacher? = dbQuery {
@@ -35,8 +41,27 @@ class TeachersDaoImpl : TeachersDao {
         }.resultedValues?.singleOrNull()?.let(::mapResultRow)
     }
 
+    override suspend fun updateTeacher(
+        teacherId: Int,
+        firstName: String,
+        lastName: String,
+        middleName: String,
+        departmentId: Int
+    ): Int = dbQuery {
+        Teachers.update(where = { Teachers.id eq teacherId }) {
+            it[Teachers.firstName] = firstName
+            it[Teachers.lastName] = lastName
+            it[Teachers.middleName] = middleName
+            it[Teachers.departmentId] = departmentId
+        }
+    }
+
     override suspend fun deleteTeacher(teacherId: Int): Boolean = dbQuery {
         Teachers.deleteWhere { Teachers.id eq teacherId } > 0
+    }
+
+    override suspend fun deleteTeachers(teacherIds: List<Int>): Boolean = dbQuery {
+        Teachers.deleteWhere { Teachers.id inList teacherIds } > 0
     }
 
     override fun mapResultRow(row: ResultRow): Teacher = Teacher.mapResultRow(row)
