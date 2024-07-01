20826133a85554fef113033477c497c41f88e8f3
Danil Nikolaev <lischenkodev@gmail.com>
2024-07-01 22:14:07 +0300

    version up

--- src/main/kotlin/com/meloda/kubsau/common/Constants.kt
+++ src/main/kotlin/com/meloda/kubsau/common/Constants.kt
@@ -2,7 +2,7 @@ package com.meloda.kubsau.common
 
 object Constants {
 
-    const val BACKEND_VERSION = "0.2.16"
+    const val BACKEND_VERSION = "0.2.17"
 }
 
 val IS_IN_DOCKER = getEnvOrElse("IS_DOCKER") { "false" }.toBooleanStrict()
